d998922b36093a90ca8f1e843fe4dc91b9c47ad4
nianqin <nianqin.vip@gmail.com>
2019-07-16 16:41:37 +0800

    update

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -45,6 +45,7 @@ module.exports = Object.assign(routerBase, {
     title: '',
     meta: [
       { charset: 'utf-8' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
     ],
     script: [
